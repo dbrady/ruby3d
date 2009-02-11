26c3bf6e8a68db8874e6728e3675101ce9a9b6aa
David Brady <dbrady@shinybit.com>
2009-02-10 21:52:52 -0700

    Implemented Matrix-Matrix, Matrix*Numeric, Matrix*Vector, Matrix*Matrix

--- lib/matrix.rb
+++ lib/matrix.rb
@@ -1,18 +1,43 @@
 # 3D matrix
+require 'vector'
+
 class Matrix
-  attr_accessor :mat
-  
-  IDENTITY = Matrix.new 1.0, 0.0, 0.0,  0.0, 1.0, 0.0,  0.0, 0.0, 1.0
+  attr_reader :rows
   
   def initialize(ax,ay,az,bx,by,bz,cx,cy,cz)
-    @mat = [ax,ay,az,bx,by,bz,cx,cy,cz]
+    @rows = [[ax,ay,az], [bx,by,bz], [cx,cy,cz]]
   end
 
   def ==(o)
-    @mat == o.mat
+    @rows.flatten == o.rows.flatten
+  end
+  
+  def cols
+    (0..2).map {|i| rows.map {|r| r[i] } }
   end
   
   def +(o)
-    Matrix.new(*(@mat.zip(o.mat).map {|a,b| a+b}))
+    Matrix.new(*(@rows.flatten.zip(o.rows.flatten).map {|a,b| a+b}))
+  end
+
+  def -(o)
+    Matrix.new(*(@rows.flatten.zip(o.rows.flatten).map {|a,b| a-b}))
+  end
+  
+  def *(o)
+    case o
+    when Numeric
+      Matrix.new(*(@rows.flatten.map {|a| a*o}))
+    when Matrix
+      Matrix.new(*((0..2).map {|a| (0..2).map {|b| Vector.new(*rows[a]).dot(Vector.new(*o.cols[b]))} }.flatten))
+    end
+  end
+
+  def /(o)
+    case o
+    when Numeric
+      Matrix.new(*(@rows.flatten.map {|a| a/o}))
+#    when Vector
+    end
   end
 end
--- spec/matrix_spec.rb
+++ spec/matrix_spec.rb
@@ -8,8 +8,39 @@ describe Matrix do
     @m2 = Matrix.new( *((1..9).map { |i| i.to_f * 2}))
   end
 
-  it "addition" do
+  it "#+" do
     (@m1 + @m1).should == @m2
   end
+
+  it "#-" do
+    (@m2 - @m1).should == @m1
+  end
+  
+  it "#*(Numeric)" do
+    (@m1 * 2).should == @m2
+  end
+  
+  it "#*(Matrix)" do
+    (@m1 * @m1).should == Matrix.new(30.0, 36.0, 42.0, 66.0, 81.0, 96.0, 102.0, 126.0, 150.0)
+    (@m1 * @identity).should == @m1
+  end
+  
+  it "#/(Numeric)" do
+    (@m2 / 2).should == @m1
+  end
+  
+  it "#rows" do
+    @m1.rows.should == [ [1.0, 2.0, 3.0], [4.0, 5.0, 6.0], [7.0, 8.0, 9.0] ]
+  end
+
+  it "#cols" do
+    @m1.cols.should == [ [1.0, 4.0, 7.0], [2.0, 5.0, 8.0], [3.0, 6.0, 9.0] ]
+  end
 end
+
+
+
+
+
+
 
